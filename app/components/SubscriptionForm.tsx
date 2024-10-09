7f1716242c7bacccd36b5908a168ad06108097e3
Your Name <ekka.beaula@gmail.com>
2024-10-09 22:52:40 +0200

    der=fine form schema

--- app/components/SubscriptionForm.tsx
+++ app/components/SubscriptionForm.tsx
@@ -1,20 +1,59 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  fullName: z.string().min(3, { message: "Name must be atleast 3 characters" }),
+  email: z.string().email({ message: "Invalid email address" }),
+});
 
 export default function SubscriptionForm() {
+  //1.define a form
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+    },
+  });
+
+  // 2.Define a submit handler
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
-    <div>
-      <form action="">
-        <Label>Full Name:</Label>
-        <Input placeholder="John Doe" />
-
-        <Label>Email:</Label>
-        <Input placeholder="John@john.com" />
-
-        <Button>Submit</Button>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
       </form>
-    </div>
+    </Form>
   );
 }
